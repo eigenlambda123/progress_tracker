168b4337cc231249220950172777810b13005f54
Rodmark "RM" Villa <rmvilla987@gmail.com>
2024-12-24 11:37:26 +0800

    Add Tracker model and API view for retrieving data but still need to add templates

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import home_page_view, test_api
+from .views import home_page_view, TestAPIView
 
 urlpatterns = [
     path("", home_page_view, name="home"),
-    path("api/test/", test_api, name="test_api"),
+    path("api/test/", TestAPIView.as_view(), name="test-api"),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Tracker
+from .serializers import TrackerSerializer
 
-@api_view(["GET"])
-def test_api(request):
-    return Response({"message": "Hello, API!"})
+class TestAPIView(APIView):
+    def get(self, request):
+        data = Tracker.objects.all()
+        serializer = TrackerSerializer(data, many=True)
+        return Response(serializer.data)
 
 
 def home_page_view(request):
