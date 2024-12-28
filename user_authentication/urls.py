c2014c1b1e15cf9886271e3d85d78f007f493aaa
Rodmark "RM" Villa <rmvilla987@gmail.com>
2024-12-28 21:18:50 +0800

    Add login, register, and logout links to the navigation bar; update URL routing for logout

--- user_authentication/urls.py
+++ user_authentication/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
    path('login/', views.login, name='login'),
    path('register/', views.register, name='register'),
+   path('logout/', views.user_logout, name='logout'),
 ]
