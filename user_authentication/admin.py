b09fed9a39df96c18a72e76d691f91363141382f
Rodmark "RM" Villa <rmvilla987@gmail.com>
2024-12-29 10:57:00 +0800

    Add Profile model and integrate with CustomUser; remove Login model

--- user_authentication/admin.py
+++ user_authentication/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Profile
 
 admin.site.register(CustomUser)
+admin.site.register(Profile)
 
-# Register your models here.
+# Mix profile with user
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
--- user_authentication/models.py
+++ user_authentication/models.py
@@ -1,11 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class CustomUser(AbstractUser):
     # Add custom fields here
-    pass
+    """
+    Custom user model that extends the default Django user model.
+    Add custom fields or methods here if needed.
+    """
 
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
 
+    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def __str__(self):
+        return self.user.username   
 
 
