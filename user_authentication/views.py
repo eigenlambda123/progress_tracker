bc713e2d84d42774e9073c84efa0bb857bbfee9a
Rodmark "RM" Villa <rmvilla987@gmail.com>
2024-12-29 10:41:01 +0800

    Update navigation links for authenticated users and remove unused Login model

--- user_authentication/views.py
+++ user_authentication/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect 
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
-from django.contrib import messages
 from .forms import LoginForm
 
 def register(request):
